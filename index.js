c40692c88879d91a5b8b1c7602f747b67e62f970
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-06-13 18:15:07 -0300

    feat: atualizar e deletar mulher

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ function showPort() {
     console.log(`Listening on port ${port}`);
 }
 
+//GET
 function showWomen(req, res) {
     res.json(women);
 }
 
+//POST
 function addWoman(req, res) {
     const newWoman = {
         id: crypto.randomUUID(),
@@ -38,6 +40,41 @@ function addWoman(req, res) {
     res.json(women);
 }
 
+//PATCH
+function updateWoman(req, res) {
+    const womanToUpdate = women.find(woman => woman.id === req.params.id);
+
+    if (!womanToUpdate) {
+        return res.status(404).json({ message: 'Mulher não encontrada' });
+    }
+    if (req.body.name) {
+        womanToUpdate.name = req.body.name;
+    }
+    if (req.body.image) {
+        womanToUpdate.image = req.body.image;
+    }
+    if (req.body.desc) {
+        womanToUpdate.desc = req.body.desc;
+    }
+    res.json(women);
+}
+
+//DELETE
+function deleteWoman(req, res) {
+    // const womanToDelete = women.find((woman) => woman.id === req.params.id);
+    // if (!womanToDelete) {
+    //     return res.status(404).json({ message: 'Mulher não encontrada' });
+    // }
+    // const index = women.indexOf(womanToDelete);
+    // women.splice(index, 1);
+    // res.json(women);
+
+    const newWomenList = women.filter(woman => woman.id !== req.params.id);
+    res.json(newWomenList);
+}
+
 app.listen(port, showPort);
-app.use(router.get('/', showWomen));
-app.use(router.post('/adicionar-mulher', addWoman));
+app.use(router.get('/mulheres', showWomen));
+app.use(router.post('/nova-mulher', addWoman));
+app.use(router.patch('/mulheres/:id', updateWoman));
+app.use(router.delete('/mulheres/:id', deleteWoman));
