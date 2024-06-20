9e2f26e6055e516ae711040d77a10107c7a0eb6a
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-06-20 18:58:09 -0300

    feat: usa dotenv para proteger string de conexão

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 async function connectDb() {
     try {
         console.log('Connecting to database');
-        await mongoose.connect(
-            'mongodb+srv://alinebevilacqua:36BUBMFWesCCRfnC@clustermulheres.3xtbljm.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMulheres'
-        );
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connection successful');
     } catch (e) {
         console.log(e);
