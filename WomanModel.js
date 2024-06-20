8ad3089b59fc032b1ec2688c8df07f975971ab53
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-06-20 18:16:48 -0300

    fix: instancia corretamente o objeto usando "new"

--- WomanModel.js
+++ WomanModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const WomanSchema = mongoose.Schema({
+const WomanSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
