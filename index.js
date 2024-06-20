905013c81ac50c90012d711f89418326071f7bc2
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-06-20 19:47:18 -0300

    feat: instala e usa pacote cors para liberar api para o frontend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const connectDb = require('./db');
 const Woman = require('./WomanModel');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const router = express.Router();
 const port = 3333;
 
